bbb1f5623c3f5d73b376ed78e98d4e4d0faba5e5
arifm <arifmho@yandex.ru>
2024-02-01 15:00:41 +0300

    Updates

--- assets/ios.transition-legacy-VGNXRvNS.js
+++ assets/ios.transition-legacy-VGNXRvNS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-RVsw9qsC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
+System.register(["./index-legacy-VqvyxdF3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
